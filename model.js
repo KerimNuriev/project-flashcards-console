c3623d54a982ce1362e35fa3519246d06c289379
Kerim <charim.nio@yandex.ru>
2023-01-27 12:00:38 +0300

    хочу подтянуть версию Никиты

--- model.js
+++ model.js
@@ -1,19 +1,19 @@
 class Model {
-    constructor(topics, questions, answers) {
-        this.topics = topics;
-        this.questions = questions;
-        this.answers = answers;
-    }
+  constructor(topics, questions, answers) {
+    this.topics = topics;
+    this.questions = questions;
+    this.answers = answers;
+  }
 
-    topic () {
-        return this.topics; 
-    }
+  topic() {
+    return this.topics;
+  }
 
-    ask () {
-        return this.questions;
-    }
+  ask() {
+    return this.questions;
+  }
 
-    answer () {
-        return this.answers;
-    }
+  answer() {
+    return this.answers;
+  }
 }
